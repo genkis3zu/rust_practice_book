fc5def15d0b3920793549cddee7a33a458ca3ee7
Genkis <79037367+genkis3zu@users.noreply.github.com>
2024-01-22 06:46:36 +0900

    進捗

---
+++ hello2/target/debug/deps/hello2.d
@@ -0,0 +1,5 @@
+C:\Users\mizuy\OneDrive\Documents\ProgramingWorkSpace\Rust\rust_practice_book\hello2\target\debug\deps\hello2.exe: src\main.rs
+
+C:\Users\mizuy\OneDrive\Documents\ProgramingWorkSpace\Rust\rust_practice_book\hello2\target\debug\deps\hello2.d: src\main.rs
+
+src\main.rs:
---
+++ hello2/target/debug/hello2.d
@@ -0,0 +1,1 @@
+C:\Users\mizuy\OneDrive\Documents\ProgramingWorkSpace\Rust\rust_practice_book\hello2\target\debug\hello2.exe: C:\Users\mizuy\OneDrive\Documents\ProgramingWorkSpace\Rust\rust_practice_book\hello2\src\main.rs
