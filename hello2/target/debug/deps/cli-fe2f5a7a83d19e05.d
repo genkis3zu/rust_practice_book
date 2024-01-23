3ff8a07718c718a71720beeee6c8a19ad1d994db
Genkis <79037367+genkis3zu@users.noreply.github.com>
2024-01-23 21:30:29 +0900

    create tests

---
+++ hello2/target/debug/deps/cli-fe2f5a7a83d19e05.d
@@ -0,0 +1,5 @@
+C:\Users\mizuy\OneDrive\Documents\ProgramingWorkSpace\Rust\rust_practice_book\hello2\target\debug\deps\cli-fe2f5a7a83d19e05.exe: tests\cli.rs
+
+C:\Users\mizuy\OneDrive\Documents\ProgramingWorkSpace\Rust\rust_practice_book\hello2\target\debug\deps\cli-fe2f5a7a83d19e05.d: tests\cli.rs
+
+tests\cli.rs:
---
+++ hello2/target/debug/deps/hello2-5784bbae0125e951.d
@@ -0,0 +1,5 @@
+C:\Users\mizuy\OneDrive\Documents\ProgramingWorkSpace\Rust\rust_practice_book\hello2\target\debug\deps\hello2-5784bbae0125e951.exe: src\main.rs
+
+C:\Users\mizuy\OneDrive\Documents\ProgramingWorkSpace\Rust\rust_practice_book\hello2\target\debug\deps\hello2-5784bbae0125e951.d: src\main.rs
+
+src\main.rs:
